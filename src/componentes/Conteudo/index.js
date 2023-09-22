a4ab89abcb75cbeca9912882ae85698697f808ec
Giovanna Liz <gigilizgadelha@gmail.com>
2023-09-22 09:42:13 -0300

    css conteudo e atualização do codigo

--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { useState } from "react";
+import styles from './Conteudo.module.css'
 
 const Conteudo = () => {
-  const [nome, setNome]=useState("Google")
+
+  const [nome, setNome]=useState("google")
     const [url, setUrl]=useState("https://www.google.com")
     const [favoritos, setFavoritos]=useState([])
 
-    function adicionarFavorito(nome, url){
-        let favorito={nome,url}
-        setFavoritos([...favoritos,favorito])
+
+
+    function adicionarFavorito(){
+        setFavoritos([...favoritos,{nome,url}]) 
+        localStorage.setItem ("favorito", {nome,url})
         console.log(favoritos)
+    
     }
 
     function handleInput(valor){
@@ -17,18 +22,25 @@ const Conteudo = () => {
 
     }
     return(
-        <div>
-            <h1>Conteudo</h1>
+        <div className={styles.area}>
+            
+            <form className={styles.formulario}>    
+        
             <h1>{nome}</h1>
-            <h1>{url}</h1>
+            <label> Nome do Site </label>
             <input type="text" value={nome} onChange={(e)=>handleInput(e.target.value)} />
             <br />
+             
+            <h1>{url}</h1>
+            <label> Url </label>
             <input type="text" value={url} onChange={(e)=>setUrl(e.target.value)} />
-            <button onClick={()=>adicionarFavorito(nome,url)}>Adicionar</button>
-            <h1>Favoritos</h1>
+
+            <input type="button" onClick={()=>adicionarFavorito(nome,url)} value="Adicionar"/>
+        
             <ul>
             { favoritos.map( (fav) => <li key={fav.url}>{ fav.nome } <br />{ fav.url }</li>) }
             </ul>
+            </form>
         </div>
     );
 };
